2d5ce2be5ec72f0f113886b6f625eb470bebdcae
Glavin Wiechert <glavin.wiechert@gmail.com>
2015-05-13 02:21:39 -0300

    See #3. Add initial Wit.ai speech recognition

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const app = require('app');
 const BrowserWindow = require('browser-window');
 const Menu = require('menu');
 const template = require(`./menu.js`);
+const ipc = require('ipc');
+const fs = require('fs');
+const path = require('path');
+const wit = require('node-wit');
+
+const WIT_ACCESS_TOKEN = "FRH5QS2T4EW5ANB3N44YUXGZLUQUCSO5";
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -35,5 +41,67 @@ app.on('ready', function() {
         // for multiple windows store them in an array
         mainWindow = null;
     });
+
+});
+
+
+
+// Speech Recognition
+
+// Declare recording in this scope
+var recording;
+
+ipc.on('speech-recognition:start', function(event, arg) {
+    console.log('speech-recognition:start');
+    if (recording) {
+        console.warn(
+            "Recording in progress. Stop previous recording first before starting another."
+        );
+    }
+
+    var callback = function(err,
+        res) {
+        console.log('captureSpeechIntentFromMic', err,
+            res);
+        // console.log(
+        //     "Response from Wit for microphone audio stream: "
+        // );
+        if (err) {
+            event.sender.send('speech-recognition:error', err);
+            // $intent.html("Error: ", err);
+        }
+        event.sender.send('speech-recognition:result', res);
+        // $intent.html(JSON.stringify(res, null, " "));
+    };
+
+
+    // Wit.ai Speech Recognition & Intent
+    // The `captureSpeechIntent` function returns the `node-record-lpcm16` object
+    // See https://github.com/gillesdemey/node-record-lpcm16 for more details
+    recording = wit.captureSpeechIntentFromMic(
+        WIT_ACCESS_TOKEN, callback);
+
+    // wit.captureTextIntent(WIT_ACCESS_TOKEN,
+    //     "this is an awesome test", callback)
+
+    // var stream = fs.createReadStream(path.resolve(__dirname,
+    //     '../node_modules/node-wit/test/resources/sample.wav'
+    // ));
+    // wit.captureSpeechIntent(WIT_ACCESS_TOKEN, stream, "audio/wav",
+    //     callback);
+
+    event.sender.send('speech-recognition:start');
+    
+});
+
+ipc.on('speech-recognition:stop', function(event, arg) {
+    console.log('speech-recognition:stop');
+    if (recording && typeof recording.stop === "function") {
+        // Stop recording
+        recording.stop();
+    }
+    recording = null;
+
+    event.sender.send('speech-recognition:stop');
 
 });
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,14 +1,20 @@
 'use strict';
+/* global $:false, document:false */
 
 $(document).ready(function() {
 
-    var Myo = require('myo');
+    // Dependencies
+    const Myo = require('myo');
+    const ipc = require('ipc');
+
     // Elements
-    var $main = $('.main');
-    var $pose = $('.pose');
-    var $gyroscope = $('.gyroscope');
-    var $orientation = $('.orientation');
-    var $accelerometer = $('.accelerometer');
+    const $main = $('.main');
+    const $intent = $('.intent');
+    const $pose = $('.pose');
+    const $gyroscope = $('.gyroscope');
+    const $orientation = $('.orientation');
+    const $accelerometer = $('.accelerometer');
+    const $listenBtn = $('.listen-btn');
 
     $main.html('Please connect your Myo');
     Myo.onError = function() {
@@ -20,31 +26,58 @@ $(document).ready(function() {
 
     myMyo.on('connected', function(data, timestamp) {
         $main.html('Hello Myo! ' + timestamp);
-        myMyo.setLockingPolicy('none');
+        myMyo.setLockingPolicy('standard');
         myMyo.vibrate();
     });
 
     myMyo.on('pose', function(poseName, edge) {
         // console.log(`Pose: ${poseName}, ${edge}`);
-        if (!edge) return;
         $pose.html(`Pose: ${poseName} ${edge} ${new Date()}`);
-        // myMyo.vibrate();
+
+        if (poseName === "fist") {
+            if (edge) {
+                ipc.send('speech-recognition:start');
+            } else {
+                ipc.send('speech-recognition:stop');
+            }
+        }
     });
 
     myMyo.on('gyroscope', function(data) {
         $gyroscope.html(
             `gyroscope: ${JSON.stringify(data, undefined, 4)}`
         );
-    })
+    });
     myMyo.on('orientation', function(data) {
         $orientation.html(
             `orientation: ${JSON.stringify(data, undefined, 4)}`
         );
-    })
+    });
     myMyo.on('accelerometer', function(data) {
         $accelerometer.html(
             `accelerometer: ${JSON.stringify(data, undefined, 4)}`
         );
-    })
+    });
+
+    ipc.on('speech-recognition:start', function() {
+        $intent.html('Listening...');
+
+        myMyo.vibrate();
+    });
+    ipc.on('speech-recognition:stop', function() {
+        $intent.html('Processing speeching...');
+        myMyo.vibrate();
+    });
+
+    ipc.on('speech-recognition:error', function(error) {
+        $intent.html(`Error: ${error}`);
+    });
+    ipc.on('speech-recognition:result', function(res) {
+        $intent.html(`Result: ${JSON.stringify(res, null, " ")}`);
+    });
+
+    $listenBtn.click(function() {
+        ipc.send('speech-recognition:start');
+    });
 
 });
