68f58f219227688178e2d2965805716b3c5aa999
Glavin Wiechert <glavin.wiechert@gmail.com>
2015-05-12 23:14:09 -0300

    See #1. Add UI for Myo pose, gyroscope, orientation, accelerometer
    Also setup Bower and add Twitter Bootstrap.

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,16 +1,50 @@
-// Dependencies
-var $ = require('jquery');
-var Myo = require('myo');
+'use strict';
 
-// Elements
-var $main = $('.main');
+$(document).ready(function() {
 
-//
-var myMyo = Myo.create();
-console.log('Waiting for Myo');
-myMyo.on('fist', function(edge){
-    console.log('fist', edge);
-    if(!edge) return;
-    $main.html('Hello Myo!');
-    myMyo.vibrate();
+    var Myo = require('myo');
+    // Elements
+    var $main = $('.main');
+    var $pose = $('.pose');
+    var $gyroscope = $('.gyroscope');
+    var $orientation = $('.orientation');
+    var $accelerometer = $('.accelerometer');
+
+    $main.html('Please connect your Myo');
+    Myo.onError = function() {
+        $main.html("Woah, couldn't connect to Myo Connect");
+    };
+
+    //
+    var myMyo = Myo.create();
+
+    myMyo.on('connected', function(data, timestamp) {
+        $main.html('Hello Myo! ' + timestamp);
+        myMyo.setLockingPolicy('none');
+        myMyo.vibrate();
+    });
+
+    myMyo.on('pose', function(poseName, edge) {
+        // console.log(`Pose: ${poseName}, ${edge}`);
+        if (!edge) return;
+        $pose.html(`Pose: ${poseName} ${edge} ${new Date()}`);
+        // myMyo.vibrate();
+    });
+
+    myMyo.on('gyroscope', function(data) {
+        $gyroscope.html(
+            `gyroscope: ${JSON.stringify(data, undefined, 4)}`
+        );
+    })
+    myMyo.on('orientation', function(data) {
+        $orientation.html(
+            `orientation: ${JSON.stringify(data, undefined, 4)}`
+        );
+    })
+    myMyo.on('accelerometer', function(data) {
+        $accelerometer.html(
+            `accelerometer: ${JSON.stringify(data, undefined, 4)}`
+        );
+    })
+
 });
